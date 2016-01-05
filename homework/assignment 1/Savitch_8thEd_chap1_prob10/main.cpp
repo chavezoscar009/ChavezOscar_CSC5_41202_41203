7f046cf547963c713a9d2d8e9865169179682273
chavezoscar009 <chavezoscar009@gmail.com>
2016-01-05 11:52:39 -0800

    Signed-off-by: chavezoscar009 <chavezoscar009@gmail.com>

---
+++ homework/assignment 1/Savitch_8thEd_chap1_prob10/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: OSCAR Chavez
+ *
+ * Created on January 5, 2016, 11:13 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    short c,b=20000,a=30000;
+    //calculate or map inputs and outputs
+    c=a+b;
+    
+    //output results
+    cout<<"c"<<endl;
+    
+    
+    
+    
+    return 0;
+}
+
